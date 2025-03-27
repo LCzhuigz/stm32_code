794fb8dee354139b2d0e4fe14ee64fe5e0fe039e
lczhuigz <lczhuigz@163.com>
2025-03-27 09:04:45 +0800

    chore: 更新 .gitignore 文件
    - 移除特定项目目录的忽略规则
    - 添加通用的 IDE、编辑器和构建相关文件忽略规则
    - 新增 CMake API 和其他临时文件的忽略规则

---
+++ F407/HAL/f407_demo_vscode/build/CMakeFiles/f407_demo_vscode.dir/DependInfo.cmake
@@ -0,0 +1,64 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "ASM"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_ASM
+  "/c/Users/Admin/Desktop/f407_demo_vscode/startup_stm32f407xx.s" "/c/Users/Admin/Desktop/f407_demo_vscode/build/CMakeFiles/f407_demo_vscode.dir/startup_stm32f407xx.s.obj"
+  )
+set(CMAKE_ASM_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_ASM
+  "DEBUG"
+  "STM32F407xx"
+  "USE_HAL_DRIVER"
+  )
+
+# The include file search paths:
+set(CMAKE_ASM_TARGET_INCLUDE_PATH
+  "/c/Users/Admin/Desktop/f407_demo_vscode/cmake/stm32cubemx/../../Core/Inc"
+  "/c/Users/Admin/Desktop/f407_demo_vscode/cmake/stm32cubemx/../../Drivers/STM32F4xx_HAL_Driver/Inc"
+  "/c/Users/Admin/Desktop/f407_demo_vscode/cmake/stm32cubemx/../../Drivers/STM32F4xx_HAL_Driver/Inc/Legacy"
+  "/c/Users/Admin/Desktop/f407_demo_vscode/cmake/stm32cubemx/../../Drivers/CMSIS/Device/ST/STM32F4xx/Include"
+  "/c/Users/Admin/Desktop/f407_demo_vscode/cmake/stm32cubemx/../../Drivers/CMSIS/Include"
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/c/Users/Admin/Desktop/f407_demo_vscode/Core/Src/gpio.c" "CMakeFiles/f407_demo_vscode.dir/Core/Src/gpio.c.obj" "gcc" "CMakeFiles/f407_demo_vscode.dir/Core/Src/gpio.c.obj.d"
+  "/c/Users/Admin/Desktop/f407_demo_vscode/Core/Src/main.c" "CMakeFiles/f407_demo_vscode.dir/Core/Src/main.c.obj" "gcc" "CMakeFiles/f407_demo_vscode.dir/Core/Src/main.c.obj.d"
+  "/c/Users/Admin/Desktop/f407_demo_vscode/Core/Src/stm32f4xx_hal_msp.c" "CMakeFiles/f407_demo_vscode.dir/Core/Src/stm32f4xx_hal_msp.c.obj" "gcc" "CMakeFiles/f407_demo_vscode.dir/Core/Src/stm32f4xx_hal_msp.c.obj.d"
+  "/c/Users/Admin/Desktop/f407_demo_vscode/Core/Src/stm32f4xx_it.c" "CMakeFiles/f407_demo_vscode.dir/Core/Src/stm32f4xx_it.c.obj" "gcc" "CMakeFiles/f407_demo_vscode.dir/Core/Src/stm32f4xx_it.c.obj.d"
+  "/c/Users/Admin/Desktop/f407_demo_vscode/Core/Src/syscalls.c" "CMakeFiles/f407_demo_vscode.dir/Core/Src/syscalls.c.obj" "gcc" "CMakeFiles/f407_demo_vscode.dir/Core/Src/syscalls.c.obj.d"
+  "/c/Users/Admin/Desktop/f407_demo_vscode/Core/Src/sysmem.c" "CMakeFiles/f407_demo_vscode.dir/Core/Src/sysmem.c.obj" "gcc" "CMakeFiles/f407_demo_vscode.dir/Core/Src/sysmem.c.obj.d"
+  "/c/Users/Admin/Desktop/f407_demo_vscode/Core/Src/system_stm32f4xx.c" "CMakeFiles/f407_demo_vscode.dir/Core/Src/system_stm32f4xx.c.obj" "gcc" "CMakeFiles/f407_demo_vscode.dir/Core/Src/system_stm32f4xx.c.obj.d"
+  "/c/Users/Admin/Desktop/f407_demo_vscode/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal.c" "CMakeFiles/f407_demo_vscode.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal.c.obj" "gcc" "CMakeFiles/f407_demo_vscode.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal.c.obj.d"
+  "/c/Users/Admin/Desktop/f407_demo_vscode/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_cortex.c" "CMakeFiles/f407_demo_vscode.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_cortex.c.obj" "gcc" "CMakeFiles/f407_demo_vscode.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_cortex.c.obj.d"
+  "/c/Users/Admin/Desktop/f407_demo_vscode/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma.c" "CMakeFiles/f407_demo_vscode.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma.c.obj" "gcc" "CMakeFiles/f407_demo_vscode.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma.c.obj.d"
+  "/c/Users/Admin/Desktop/f407_demo_vscode/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma_ex.c" "CMakeFiles/f407_demo_vscode.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma_ex.c.obj" "gcc" "CMakeFiles/f407_demo_vscode.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma_ex.c.obj.d"
+  "/c/Users/Admin/Desktop/f407_demo_vscode/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_exti.c" "CMakeFiles/f407_demo_vscode.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_exti.c.obj" "gcc" "CMakeFiles/f407_demo_vscode.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_exti.c.obj.d"
+  "/c/Users/Admin/Desktop/f407_demo_vscode/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash.c" "CMakeFiles/f407_demo_vscode.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash.c.obj" "gcc" "CMakeFiles/f407_demo_vscode.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash.c.obj.d"
+  "/c/Users/Admin/Desktop/f407_demo_vscode/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ex.c" "CMakeFiles/f407_demo_vscode.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ex.c.obj" "gcc" "CMakeFiles/f407_demo_vscode.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ex.c.obj.d"
+  "/c/Users/Admin/Desktop/f407_demo_vscode/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ramfunc.c" "CMakeFiles/f407_demo_vscode.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ramfunc.c.obj" "gcc" "CMakeFiles/f407_demo_vscode.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ramfunc.c.obj.d"
+  "/c/Users/Admin/Desktop/f407_demo_vscode/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_gpio.c" "CMakeFiles/f407_demo_vscode.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_gpio.c.obj" "gcc" "CMakeFiles/f407_demo_vscode.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_gpio.c.obj.d"
+  "/c/Users/Admin/Desktop/f407_demo_vscode/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr.c" "CMakeFiles/f407_demo_vscode.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr.c.obj" "gcc" "CMakeFiles/f407_demo_vscode.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr.c.obj.d"
+  "/c/Users/Admin/Desktop/f407_demo_vscode/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr_ex.c" "CMakeFiles/f407_demo_vscode.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr_ex.c.obj" "gcc" "CMakeFiles/f407_demo_vscode.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr_ex.c.obj.d"
+  "/c/Users/Admin/Desktop/f407_demo_vscode/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc.c" "CMakeFiles/f407_demo_vscode.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc.c.obj" "gcc" "CMakeFiles/f407_demo_vscode.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc.c.obj.d"
+  "/c/Users/Admin/Desktop/f407_demo_vscode/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc_ex.c" "CMakeFiles/f407_demo_vscode.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc_ex.c.obj" "gcc" "CMakeFiles/f407_demo_vscode.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc_ex.c.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
